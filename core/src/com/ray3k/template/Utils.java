b74d19fc8243911c09f91435c9cc095e7d4b6c13
Raeleus <raymond.ray3k@gmail.com>
2019-09-17 09:34:58 -0700

    Added utility methods.

--- core/src/com/ray3k/template/Utils.java
+++ core/src/com/ray3k/template/Utils.java
@@ -1,8 +1,18 @@
 package com.ray3k.template;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Cursor;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.EarClippingTriangulator;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.FloatArray;
+import com.badlogic.gdx.utils.ShortArray;
+import com.esotericsoftware.spine.SkeletonBounds;
 
 public class Utils {
     public static Array<Actor> getActorsRecursive(Actor actor) {
@@ -18,5 +28,71 @@ public class Utils {
         }
         
         return actors;
+    }
+    
+    private static EarClippingTriangulator earClippingTriangulator = new EarClippingTriangulator();
+    private static FloatArray floatArray = new FloatArray();
+    
+    public static float[] skeletonBoundsToTriangles(SkeletonBounds skeletonBounds) {
+        floatArray.clear();
+        
+        for (FloatArray points : skeletonBounds.getPolygons()) {
+            ShortArray shortArray = earClippingTriangulator.computeTriangles(points);
+            for (int i = 0; i < shortArray.size; i++) {
+                floatArray.add(shortArray.get(i));
+            }
+        }
+        return floatArray.items;
+    }
+    
+    public static Color inverseColor(Color color) {
+        return new Color(1 - color.r, 1 - color.g, 1 - color.b, color.a);
+    }
+    
+    public static Color blackOrWhiteBgColor(Color color) {
+        return brightness(color) > .5f ? new Color(Color.BLACK) : new Color(Color.WHITE);
+    }
+    
+    public static float brightness(Color color) {
+        return (float) (Math.sqrt(0.299f * Math.pow(color.r, 2) + 0.587 * Math.pow(color.g, 2) + 0.114 * Math.pow(color.b, 2)));
+    }
+    
+    public static int colorToInt(Color color) {
+        return ((int)(255 * color.r) << 24) | ((int)(255 * color.g) << 16) | ((int)(255 * color.b) << 8) | ((int)(255 * color.a));
+    }
+    
+    public static float floorPot(float value) {
+        float returnValue = 0.0f;
+        for (float newValue = 2.0f; newValue < value; newValue *= 2.0f) {
+            returnValue = newValue;
+        }
+        
+        return returnValue;
+    }
+    
+    public static Pixmap textureRegionToPixmap(TextureRegion textureRegion) {
+        Texture texture = textureRegion.getTexture();
+        if (!texture.getTextureData().isPrepared()) {
+            texture.getTextureData().prepare();
+        }
+        
+        Pixmap pixmap = texture.getTextureData().consumePixmap();
+        Pixmap returnValue = new Pixmap(textureRegion.getRegionWidth(), textureRegion.getRegionHeight(), Pixmap.Format.RGBA8888);
+        returnValue.setBlending(Pixmap.Blending.None);
+        
+        for (int x = 0; x < textureRegion.getRegionWidth(); x++) {
+            for (int y = 0; y < textureRegion.getRegionHeight(); y++) {
+                int colorInt = pixmap.getPixel(textureRegion.getRegionX() + x, textureRegion.getRegionY() + y);
+                returnValue.drawPixel(x, y, colorInt);
+            }
+        }
+        
+        pixmap.dispose();
+        
+        return returnValue;
+    }
+    
+    public static Cursor textureRegionToCursor(TextureRegion textureRegion, int xHotspot, int yHotspot) {
+        return Gdx.graphics.newCursor(textureRegionToPixmap(textureRegion), xHotspot, yHotspot);
     }
 }
